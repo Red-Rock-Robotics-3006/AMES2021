0cabdb3b871688044883d9c33f450291b257ca9f
42ndPerson <74685713+42ndPerson@users.noreply.github.com>
2021-12-09 21:29:10 -0700

    Improved and fixed bugs in GrabberSubsystem

--- FRC2021/src/main/java/frc/robot/subsystems/GrabberSubsystem.java
+++ FRC2021/src/main/java/frc/robot/subsystems/GrabberSubsystem.java
@@ -8,7 +8,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Constants.DriveConstants;
 
 public class GrabberSubsystem extends SubsystemBase {
@@ -17,7 +19,7 @@ public class GrabberSubsystem extends SubsystemBase {
 
   private PIDController pid = new PIDController(0d, 0d, 0d);
 
-  private int openState = 0; //0 is closed; 1 is open; 2 is inbetween
+  private int state = 0; //0 is closed; 1 is open; 2 is inbetween
   private boolean isEased; //Functionality to be added later with PID
 
   private final double OPEN_POS = 0.4;
@@ -40,65 +42,95 @@ public class GrabberSubsystem extends SubsystemBase {
   {
     this.liftMotor.getSensorCollection().setIntegratedSensorPosition(0, 0);
   }
+  /**
+   * @param power to move with (positive is raising and negative is lowering)
+   */
   private void move(double power)
   {
     this.liftMotor.set(ControlMode.PercentOutput, power);
-      
   }
-  private boolean reachedLimit()
+  private double getPower()
   {
-    if(this.getPosition() >= this.OPEN_POS) 
+    return this.liftMotor.get();
+  }
+  private void limitCheck()
+  {
+    if(this.getPosition() <= this.CLOSED_POS) this.state = 0;
+    else if(this.getPosition() >= this.OPEN_POS) this.state = 1;
+    else this.state = 2;
+  }
+  /**
+   * @return true for safe and false for unsafe
+   */
+  private boolean safetyCheck()
+  {
+    if((this.state == 0 && getPower() < 0) || (this.state == 1 && getPower() > 0)) 
     {
-      this.openState = 1;
-      return true;
+      this.stop();
+      return false;
     }
-    if (this.getPosition() <= this.CLOSED_POS)
-    {
-      this.openState = 0;
-      return true; 
-    }
-    this.openState = 2;
-    return false; 
+    return true;
   }
 
-  public boolean moveToPosition(double power)
+  /**
+   * @param power to move with (positive is raising and negative is lowering)
+   */
+  public void moveToPosition(double power)
   {
-    if(reachedLimit()){
-      this.stop();
-      return true;
-    } else {
-      if (!isEased) this.move(power);
-      else { //Currently does not ease; Just for debug
+    if(safetyCheck()) 
+    {
+      if(!isEased) this.move(power);
+      else 
+      { //Currently does not ease; Just for debug
         System.out.println("Move: " + power);
-        System.out.println("PID: " + MathUtil.clamp(
-          pid.calculate( //Check if fractional position can be used like this or if raw position must be used
+        System.out.println(
+          "PID: " + 
+          MathUtil.clamp(
+            pid.calculate( //Check if fractional position can be used like this or if raw position must be used
               this.getPosition(),
               power < 0 ? this.CLOSED_POS : this.OPEN_POS
-            )
-          ),
-          -power,
-          power
+            ),
+            -power,
+            power
+          )
         );
         this.move(power);
       }
-      return false; 
     }
   }
   public void stop() {
     this.move(0);
   }
-  public int getOpenState() 
+  /**
+   * @return 0 for closed, 1 for open, and 2 for inbetween
+   */
+  public int getState() 
   {
-    return this.openState;
+    return this.state;
+  }
+  /**
+   * @return 0 for closed, 1 for open, and 2 for inbetween
+   */
+  public int getTarget()
+  {
+    if(getPower() < 0) return 0;
+    if(getPower() > 0) return 1;
+    if(getPower() == 0) return 2;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(getPower() != 0) //For efficiency since method is called in periodic
+    {
+      limitCheck();
+      safetyCheck();
+    }
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+    periodic();
   }
 }
